001e6ebbcee220a5d32db75a6ada1fc027c6f8b2
Ixales <alexis.ardourel2003@gmail.com>
2022-02-07 15:52:50 +0100

    1

--- src/Tableau.js
+++ src/Tableau.js
@@ -46,107 +46,151 @@ class Tableau extends Phaser.Scene {
 
         this.brique1 = this.physics.add.sprite(120, 200,'carre').setOrigin(0, 0);
         this.brique1.setDisplaySize(60,30);
-        this.brique1.body.immovable = true;
+        this.brique1.body.setAllowGravity(false);
         this.brique2 = this.physics.add.sprite(181, 200,'carre').setOrigin(0, 0);
         this.brique2.setDisplaySize(60,30);
+        this.brique2.body.setAllowGravity(false);
         this.brique3 = this.physics.add.sprite(242, 200,'carre').setOrigin(0, 0);
         this.brique3.setDisplaySize(60,30);
+        this.brique3.body.setAllowGravity(false);
         this.brique4 = this.physics.add.sprite(303, 200,'carre').setOrigin(0, 0);
         this.brique4.setDisplaySize(60,30);
+        this.brique4.body.setAllowGravity(false);
         this.brique5 = this.physics.add.sprite(364, 200,'carre').setOrigin(0, 0);
         this.brique5.setDisplaySize(60,30);
+        this.brique5.body.setAllowGravity(false);
         this.brique6 = this.physics.add.sprite(425, 200,'carre').setOrigin(0, 0);
         this.brique6.setDisplaySize(60,30);
+        this.brique6.body.setAllowGravity(false);
         this.brique7 = this.physics.add.sprite(486, 200,'carre').setOrigin(0, 0);
         this.brique7.setDisplaySize(60,30);
+        this.brique7.body.setAllowGravity(false);
         this.brique8 = this.physics.add.sprite(547, 200,'carre').setOrigin(0, 0);
         this.brique8.setDisplaySize(60,30);
+        this.brique8.body.setAllowGravity(false);
         this.brique9 = this.physics.add.sprite(608, 200,'carre').setOrigin(0, 0);
         this.brique9.setDisplaySize(60,30);
+        this.brique9.body.setAllowGravity(false);
 
         //Briques rangée2
 
         this.brique1 = this.physics.add.sprite(120, 231,'carre').setOrigin(0, 0);
         this.brique1.setDisplaySize(60,30);
+        this.brique1.body.setAllowGravity(false);
         this.brique2 = this.physics.add.sprite(181, 231,'carre').setOrigin(0, 0);
         this.brique2.setDisplaySize(60,30);
+        this.brique2.body.setAllowGravity(false);
         this.brique3 = this.physics.add.sprite(242, 231,'carre').setOrigin(0, 0);
         this.brique3.setDisplaySize(60,30);
+        this.brique3.body.setAllowGravity(false);
         this.brique4 = this.physics.add.sprite(303, 231,'carre').setOrigin(0, 0);
         this.brique4.setDisplaySize(60,30);
+        this.brique4.body.setAllowGravity(false);
         this.brique5 = this.physics.add.sprite(364, 231,'carre').setOrigin(0, 0);
         this.brique5.setDisplaySize(60,30);
+        this.brique5.body.setAllowGravity(false);
         this.brique6 = this.physics.add.sprite(425, 231,'carre').setOrigin(0, 0);
         this.brique6.setDisplaySize(60,30);
+        this.brique6.body.setAllowGravity(false);
         this.brique7 = this.physics.add.sprite(486, 231,'carre').setOrigin(0, 0);
         this.brique7.setDisplaySize(60,30);
+        this.brique7.body.setAllowGravity(false);
         this.brique8 = this.physics.add.sprite(547, 231,'carre').setOrigin(0, 0);
         this.brique8.setDisplaySize(60,30);
+        this.brique8.body.setAllowGravity(false);
         this.brique9 = this.physics.add.sprite(608, 231,'carre').setOrigin(0, 0);
         this.brique9.setDisplaySize(60,30);
+        this.brique9.body.setAllowGravity(false);
 
         //Briques rangée3
 
         this.brique1 = this.physics.add.sprite(120, 262,'carre').setOrigin(0, 0);
         this.brique1.setDisplaySize(60,30);
+        this.brique1.body.setAllowGravity(false);
         this.brique2 = this.physics.add.sprite(181, 262,'carre').setOrigin(0, 0);
         this.brique2.setDisplaySize(60,30);
+        this.brique2.body.setAllowGravity(false);
         this.brique3 = this.physics.add.sprite(242, 262,'carre').setOrigin(0, 0);
         this.brique3.setDisplaySize(60,30);
+        this.brique3.body.setAllowGravity(false);
         this.brique4 = this.physics.add.sprite(303, 262,'carre').setOrigin(0, 0);
         this.brique4.setDisplaySize(60,30);
+        this.brique4.body.setAllowGravity(false);
         this.brique5 = this.physics.add.sprite(364, 262,'carre').setOrigin(0, 0);
         this.brique5.setDisplaySize(60,30);
+        this.brique5.body.setAllowGravity(false);
         this.brique6 = this.physics.add.sprite(425, 262,'carre').setOrigin(0, 0);
         this.brique6.setDisplaySize(60,30);
+        this.brique6.body.setAllowGravity(false);
         this.brique7 = this.physics.add.sprite(486, 262,'carre').setOrigin(0, 0);
         this.brique7.setDisplaySize(60,30);
+        this.brique7.body.setAllowGravity(false);
         this.brique8 = this.physics.add.sprite(547, 262,'carre').setOrigin(0, 0);
         this.brique8.setDisplaySize(60,30);
+        this.brique8.body.setAllowGravity(false);
         this.brique9 = this.physics.add.sprite(608, 262,'carre').setOrigin(0, 0);
         this.brique9.setDisplaySize(60,30);
+        this.brique9.body.setAllowGravity(false);
 
         //Briques rangée4
 
         this.brique1 = this.physics.add.sprite(120, 293,'carre').setOrigin(0, 0);
         this.brique1.setDisplaySize(60,30);
+        this.brique1.body.setAllowGravity(false);
         this.brique2 = this.physics.add.sprite(181, 293,'carre').setOrigin(0, 0);
         this.brique2.setDisplaySize(60,30);
+        this.brique2.body.setAllowGravity(false);
         this.brique3 = this.physics.add.sprite(242, 293,'carre').setOrigin(0, 0);
         this.brique3.setDisplaySize(60,30);
+        this.brique3.body.setAllowGravity(false);
         this.brique4 = this.physics.add.sprite(303, 293,'carre').setOrigin(0, 0);
         this.brique4.setDisplaySize(60,30);
+        this.brique4.body.setAllowGravity(false);
         this.brique5 = this.physics.add.sprite(364, 293,'carre').setOrigin(0, 0);
         this.brique5.setDisplaySize(60,30);
+        this.brique5.body.setAllowGravity(false);
         this.brique6 = this.physics.add.sprite(425, 293,'carre').setOrigin(0, 0);
         this.brique6.setDisplaySize(60,30);
+        this.brique6.body.setAllowGravity(false);
         this.brique7 = this.physics.add.sprite(486, 293,'carre').setOrigin(0, 0);
         this.brique7.setDisplaySize(60,30);
+        this.brique7.body.setAllowGravity(false);
         this.brique8 = this.physics.add.sprite(547, 293,'carre').setOrigin(0, 0);
         this.brique8.setDisplaySize(60,30);
+        this.brique8.body.setAllowGravity(false);
         this.brique9 = this.physics.add.sprite(608, 293,'carre').setOrigin(0, 0);
         this.brique9.setDisplaySize(60,30);
+        this.brique9.body.setAllowGravity(false);
 
         //Briques rangée5
 
         this.brique1 = this.physics.add.sprite(120, 324,'carre').setOrigin(0, 0);
         this.brique1.setDisplaySize(60,30);
+        this.brique1.body.setAllowGravity(false);
         this.brique2 = this.physics.add.sprite(181, 324,'carre').setOrigin(0, 0);
         this.brique2.setDisplaySize(60,30);
+        this.brique2.body.setAllowGravity(false);
         this.brique3 = this.physics.add.sprite(242, 324,'carre').setOrigin(0, 0);
         this.brique3.setDisplaySize(60,30);
+        this.brique3.body.setAllowGravity(false);
         this.brique4 = this.physics.add.sprite(303, 324,'carre').setOrigin(0, 0);
         this.brique4.setDisplaySize(60,30);
+        this.brique4.body.setAllowGravity(false);
         this.brique5 = this.physics.add.sprite(364, 324,'carre').setOrigin(0, 0);
         this.brique5.setDisplaySize(60,30);
+        this.brique5.body.setAllowGravity(false);
         this.brique6 = this.physics.add.sprite(425, 324,'carre').setOrigin(0, 0);
         this.brique6.setDisplaySize(60,30);
+        this.brique6.body.setAllowGravity(false);
         this.brique7 = this.physics.add.sprite(486, 324,'carre').setOrigin(0, 0);
         this.brique7.setDisplaySize(60,30);
+        this.brique7.body.setAllowGravity(false);
         this.brique8 = this.physics.add.sprite(547, 324,'carre').setOrigin(0, 0);
         this.brique8.setDisplaySize(60,30);
+        this.brique8.body.setAllowGravity(false);
         this.brique9 = this.physics.add.sprite(608, 324,'carre').setOrigin(0, 0);
         this.brique9.setDisplaySize(60,30);
+        this.brique9.body.setAllowGravity(false);
 
         //Collision
         this.physics.add.collider(this.balle,this.murGauche);
